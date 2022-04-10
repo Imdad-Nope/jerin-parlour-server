0e0d64b09ae814a3866ed6ff07297af688b87b5e
Imdad-Nope <mdalauddinovi62@gmail.com>
2022-04-10 12:00:26 +0600

    admin panel is done

--- index.js
+++ index.js
@@ -3,16 +3,43 @@ const app = express()
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
+const admin = require("firebase-admin");
 const ObjectId = require('mongodb').ObjectId;
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_PRIVATE_KEY);
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n5vc4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+
+async function verifyToken(req, res, next) {
+
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers?.authorization.split(' ')[1]
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token)
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+    }
+
+
+    next();
+}
 
 async function run() {
     try {
@@ -40,7 +67,7 @@ async function run() {
             // console.log(result)
         })
         // Receive appointments by Get
-        app.get("/appointments", async (req, res) => {
+        app.get("/appointments", verifyToken, async (req, res) => {
             const email = req.query.email;
             const date = new Date(req.query.date).toLocaleDateString()
             const query = { email: email, date: date };
@@ -75,6 +102,38 @@ async function run() {
             const resultData = await usersCollection.updateOne(filter, updateDoc, options)
             // console.log(resultData);
             res.json(resultData)
+        });
+
+        // Make Role with Put
+        app.put("/users/admin", verifyToken, async (req, res) => {
+            const user = req.body;
+            const requester = req.decodedEmail;
+            if (requester) {
+                const requesterAccount = await usersCollection.findOne({ email: requester });
+                if (requesterAccount.role === 'admin') {
+                    const filter = { email: user.email }
+                    const updateDoc = { $set: { role: 'admin' } };
+                    const result = await usersCollection.updateOne(filter, updateDoc);
+                    // console.log(result);
+                    res.json(result)
+                }
+            }
+            else {
+                res.status(403).json({ messages: 'Sorry you are not allowed to make Admin' })
+            }
+
+        });
+
+        // Make Admin with Get
+        app.get("/users/:email", async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const result = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (result?.role == 'admin') {
+                isAdmin = true;
+            };
+            res.json({ admin: isAdmin })
         })
 
     }
